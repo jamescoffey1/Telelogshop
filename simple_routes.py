f8b8adce30563efb16734f573883fca37d92344d
YourName <youremail@example.com>
2025-09-29 14:31:01 -0500

    Initial commit

--- simple_routes.py
+++ simple_routes.py
@@ -9,6 +9,8 @@ from config import Config
 from utils import admin_required, format_currency
 from datetime import datetime, timedelta
 
+def register_routes(app)
+import logging
 logger = logging.getLogger(__name__)
 
 @app.route('/')
@@ -23,7 +25,6 @@ def index():
                          categories=categories,
                          shop_name=Config.SHOP_NAME)
 
-def register_routes(app):
     @app.route(f'/telegram-webhook/{Config.WEBHOOK_SECRET}', methods=['POST'])
     def telegram_webhook():
         """Handle Telegram webhook"""
